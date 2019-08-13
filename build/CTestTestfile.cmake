27215b8b999dc1df36d6c6aca00e8fc28eb5b5f2
Rajesh <rajesh.s@asimovrobotics.com>
2019-08-13 10:41:29 +0530

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/rajesh/Udemy_Course_Luqman/src
+# Build directory: /home/rajesh/Udemy_Course_Luqman/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(custom_bot)
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/rajesh/Udemy_Course_Luqman/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/rajesh/Turtlebot_WS/devel;/home/rajesh/ROS_Workspace/devel;/opt/ros/kinetic".split(';') if "/home/rajesh/Turtlebot_WS/devel;/home/rajesh/ROS_Workspace/devel;/opt/ros/kinetic" != "" else []
